e5ecbff43278f79b0f492d97cf504796cdf102b2
黄骞 <huangqian866@163.com>
2018-11-05 22:24:38 +0800

    1. 在结构体重增加了结构体的方法的例子 2. 增加了print宏打印结构体实例的例子

--- examples/structs.rs
+++ examples/structs.rs
@@ -29,6 +29,20 @@ fn main() {
     let username = String::from("huangqian");
     let u3 = build_user(email, username);
     print_user(&u3);
+
+    let u4 = User{
+        email: String::from("another@example.com"),
+        //结构体更新语法，..语法制定了剩余未显示设置值的字段应与给定的实例对应的字段相同的值。
+        ..u3
+    };
+    print_user(&u4);
+
+    //元组结构体
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    print_struct_example();
+
 }
 
 //定义结构体需要使用struct关键字并为结构体提供一个名字。
@@ -59,3 +73,33 @@ fn print_user(user: &User){
              user.sign_in_count);
 }
 
+//元组结构体。元组结构体有着结构体名称提供的含义，但没有具体的字段名，只有字段的类型。
+//当你想给整个元组取一个名字，并是元组成为与其他元组不同的类型时，元组结构体是很有用的，这时像常规结构体那样为每个字段命名就显得多余和形式化了
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+#[derive(Debug)]
+struct Rectangle{
+    width: u32,
+    height: u32,
+}
+
+//方法和函数类似：它们使用fn关键字和名称声明，可以拥有参数与返回值，同时包含在某处调用该方法时会执行的代码。
+//不过方法和函数是不同的，因为他们在结构体的上下文中被定义，并且它们第一参数总是self，它代表调用该方法的结构体实例
+
+impl Rectangle{
+
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+}
+
+fn print_struct_example(){
+    let rect1 = Rectangle{width: 30, height: 50};
+    println!("rect1 is {:?}", rect1);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
